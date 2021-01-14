3eba34e123641fe985ddc95c777f26d11378ba02
chise <chise123@live.com>
2021-01-14 08:58:58 +0800

    解决环境变量的问题

--- fast_tmp/conf/__init__.py
+++ fast_tmp/conf/__init__.py
@@ -8,6 +8,7 @@
 @info    :
 """
 import os
+import sys
 
 from . import global_settings
 
@@ -19,18 +20,22 @@ class Settings:
     def __init__(self):
         settings_module = os.environ.get(FASTAPI_VARIABLE)
         if not settings_module:
-            raise ImportError(
-                "Settings are not configured. "
-                "You must either define the environment variable %s "
-                "or call settings.configure() before accessing settings." % (FASTAPI_VARIABLE,)
-            )
+            work_path = os.getcwd()
+            path_list = os.path.split(work_path)
+            if path_list[-1] == 'fast-tmp':
+                settings_module = "fast_tmp.settings"
+            elif not os.path.isfile(os.path.join(path_list[-1], path_list[-1].replate("-", "_"), 'settings.py')):
+                raise ImportError(
+                    "未找到settings.py"
+                    f"你必须设置环境变量{FASTAPI_VARIABLE}=你的settings.py的位置"
+                    "或者在工作目录增加一个与工作目录同名的子目录并在里面增加settings.py"
+                )
+            else:
+                settings_module = path_list[-1].replace("-", "_") + ".settings"
         for setting in dir(global_settings):
             if setting.isupper():
                 setattr(self, setting, getattr(global_settings, setting))
-
-        # store the settings module in case someone later cares
         self.SETTINGS_MODULE = settings_module
-
         mod = importlib.import_module(self.SETTINGS_MODULE)
         for setting in dir(mod):
             if setting.isupper():
