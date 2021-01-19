81cfa9a317a11a6be73695916e1043185ed8f437
yuxiaofei <531189371@qq.com>
2021-01-19 14:53:24 +0800

    增加创建超级用户的指令

--- fast_cli/__init__.py
+++ fast_cli/__init__.py
@@ -1,7 +1,12 @@
 import asyncio
 import datetime
-
+import dotenv
 import typer
+import os
+
+from tortoise import Tortoise
+
+from fast_tmp.conf import settings
 
 app = typer.Typer()
 
@@ -22,6 +27,10 @@ def goodbye(name: str, formal: bool = False):
 
 @app.command()
 def access_token():
+    """
+    获取token
+    :return:
+    """
     from fast_tmp.models import User
     from fast_tmp.utils.token import create_access_token
     async def get_superuser():
@@ -38,5 +47,26 @@ def access_token():
     asyncio.run(get_superuser())
 
 
-if __name__ == "__main__":
+@app.command()
+def create_superuser(username: str, password: str):
+    """
+    创建超级用户
+    """
+    from fast_tmp.models import User
+    async def create_user(username, password):
+        dotenv.load_dotenv()
+        await Tortoise.init(config=settings.TORTOISE_ORM)
+        user = User(username=username)
+        user.set_password(password)
+        await user.save()
+        await Tortoise.close_connections()
+        typer.echo("创建成功")
+    asyncio.run(create_user(username, password))
+
+
+def main():
     app()
+
+
+if __name__ == "__main__":
+    main()
