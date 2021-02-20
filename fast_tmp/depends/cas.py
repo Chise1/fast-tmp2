7a0766d82b8c1d0cb92678794cd2c5c9cfd4c23c
Chise1 <chise123@love.com>
2021-02-20 11:10:39 +0800

    增加cas依赖功能

--- fast_tmp/depends/cas.py
+++ fast_tmp/depends/cas.py
@@ -1,11 +1,19 @@
 import importlib
+from typing import Any, List, Optional
 
 from cas import CASClient
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError
 from starlette.requests import Request
 
 from fast_tmp.conf import settings
+from fast_tmp.models import User
 from fast_tmp.responses import FastTmpRedirectResponse
+from fast_tmp.utils.token import decode_access_token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=settings.FAST_TMP_URL + "/auth/token")
 
 cas_client = CASClient(
     version=3,
@@ -16,7 +24,7 @@ cas_client = CASClient(
 )
 
 
-async def cas_get_user(request: Request):
+async def cas_get_user(request: Request) -> str:
     """
     获取用户
     """
@@ -25,7 +33,7 @@ async def cas_get_user(request: Request):
         raise FastTmpRedirectResponse(
             f"{request.base_url}{settings.CAS_LOGIN_URL[1:]}?next={request.url}"
         )
-    return user
+    return user["username"]
 
 
 async def cas_middleware(request: Request, call_next):
@@ -60,3 +68,57 @@ async def cas_middleware(request: Request, call_next):
     else:
         response = await call_next(request)
         return response
+
+
+async def get_user(username: str = Depends(cas_get_user)) -> Optional[User]:
+    user = await User.filter(username=username).first()
+    return user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = decode_access_token(token)
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = await get_user(username=username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if not current_user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+async def get_superuser(current_user: User = Depends(get_current_active_user)):
+    if not current_user.is_superuser:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    return current_user
+
+
+def get_user_has_perms(perms: List[Any]):
+    """
+    判定用户是否具有相关权限
+    """
+
+    async def user_has_perms(user: User = Depends(get_current_active_user)):
+        if await user.has_perms(perms):
+            return user
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+    return user_has_perms
