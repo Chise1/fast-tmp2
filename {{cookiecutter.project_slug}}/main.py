4b3001840d9fd708d7fee67fbb73b3f9bf8ae0f9
yuxiaofei <531189371@qq.com>
2021-02-26 09:27:05 +0800

    测试cookiecutter2

---
+++ {{cookiecutter.project_slug}}/main.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : main.py
+@Time    : 2021/2/26 9:26
+@Author  : chise
+@Email   : chise123@live.com
+@Software: PyCharm
+@info    :
+"""
+if __name__ == '__main__':
+    print("Hello CookieCuttter")
