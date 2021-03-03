2362ed01982cc41d332c551e84b83837c4fad323
yuxiaofei <531189371@qq.com>
2021-03-03 11:16:11 +0800

    删除测试数据

--- tests/test_cli/fast_t/fast_t/apps/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from fast_tmp.amis_app import AmisAPI
-from fast_tmp.conf import settings
-from fast_t.apps.api.routes.hello_fast_tmp import hello_fast_tmp_router
-fast_t_app = AmisAPI(
-    title="api",
-    debug=settings.DEBUG,
-)
-app.include_router(hello_fast_tmp_router)
--- tests/test_cli/fast_t/fast_t/apps/api/routes/hello_fast_tmp.py
+++
@@ -1,8 +0,0 @@
-from fast_tmp.amis_router import AmisRouter
-
-hello_fast_tmp_router = AmisRouter(title="示例路由", prefix="/test")
-
-
-@hello_fast_tmp_router.get("/hello")
-async def hello_fast_mtp():
-    return "你好,fast-tmp."
