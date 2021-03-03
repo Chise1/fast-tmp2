64b3d51a0b0c5e679f2d427b1bb4ed98a836b762
yuxiaofei <531189371@qq.com>
2021-03-03 11:14:35 +0800

    修复startapp的bug

--- fast_cli/tpl/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
+++ fast_cli/tpl/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
@@ -1,7 +1,7 @@
 import os
 
 os.environ.setdefault("FASTAPI_SETTINGS_MODULE", '{{cookiecutter.project_slug}}.settings')
-from example.factory import create_app
+from {{cookiecutter.project_slug}}.factory import create_app
 
 app = create_app()
 
